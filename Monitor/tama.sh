3eb3be44ca57309eb7ac57fc42685f08317ccea3
陳鈞 <jim60105@gmail.com>
2021-01-24 01:08:53 +0800

    將log送至log server

--- Monitor/tama.sh
+++ Monitor/tama.sh
@@ -3,5 +3,10 @@
 # Make folder if not exist
 mkdir -p /youtube-dl/logs
 
-#Change the URL link and the name of the log file.
-nohup /bin/bash live-dl https://www.youtube.com/channel/UCBC7vYFNQoGPupe5NxPG4Bw &>/youtube-dl/logs/live-dl-tama.$(date +%d%b%y-%H%M%S).log &
+# Change the URL link and the name of the log file.
+#
+# File logging:
+# nohup /bin/bash live-dl https://www.youtube.com/channel/UCBC7vYFNQoGPupe5NxPG4Bw &>/youtube-dl/logs/live-dl-tama.$(date +%d%b%y-%H%M%S).log &
+#
+# STDOUT logging:
+nohup /bin/bash live-dl https://www.youtube.com/channel/UCBC7vYFNQoGPupe5NxPG4Bw &
