85c4843d2969754a771defed7ca344006c662e39
陳鈞 <jim60105@gmail.com>
2021-01-24 21:22:04 +0800

    live-dl更新，添加支援log tag

---
+++ Monitor/gcq.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# File logging:
+#
+# mkdir -p /youtube-dl/logs
+# nohup /bin/bash live-dl https://www.youtube.com/channel/UC01vkmFjimJGiyNZkU5dyvw &>/youtube-dl/logs/live-dl.$(date +%d%b%y-%H%M%S).log &
+
+#
+# STDOUT logging (with color):
+#
+# nohup /bin/bash live-dl https://www.youtube.com/channel/UC01vkmFjimJGiyNZkU5dyvw --log-color &
+
+#
+# STDOUT logging (with log tag):
+#
+nohup /bin/bash live-dl https://www.youtube.com/channel/UC01vkmFjimJGiyNZkU5dyvw --log-tag GCQ &
--- Monitor/tama.sh
+++ Monitor/tama.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Make folder if not exist
-mkdir -p /youtube-dl/logs
-
-# Change the URL link and the name of the log file.
 #
 # File logging:
-# nohup /bin/bash live-dl https://www.youtube.com/channel/UCBC7vYFNQoGPupe5NxPG4Bw &>/youtube-dl/logs/live-dl-tama.$(date +%d%b%y-%H%M%S).log &
 #
-# STDOUT logging:
-nohup /bin/bash live-dl https://www.youtube.com/channel/UCBC7vYFNQoGPupe5NxPG4Bw &
+# mkdir -p /youtube-dl/logs
+# nohup /bin/bash live-dl https://www.youtube.com/channel/UCBC7vYFNQoGPupe5NxPG4Bw &>/youtube-dl/logs/live-dl.$(date +%d%b%y-%H%M%S).log &
+
+#
+# STDOUT logging (with color):
+#
+# nohup /bin/bash live-dl https://www.youtube.com/channel/UCBC7vYFNQoGPupe5NxPG4Bw --log-color &
+
+#
+# STDOUT logging (with log tag):
+#
+nohup /bin/bash live-dl https://www.youtube.com/channel/UCBC7vYFNQoGPupe5NxPG4Bw --log-tag Tama &
