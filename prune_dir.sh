5f2db87f467c1effc0bddaa0da84583409520eb5
陳鈞 <jim60105@gmail.com>
2022-09-15 19:50:48 +0800

    修正prune_dir中find的用法

--- prune_dir.sh
+++ prune_dir.sh
@@ -5,43 +5,33 @@
 DIR=$1
 CAPACITY_LIMIT=$2
 
-if [ "$DIR" == "" ]
-then
+if [ "$DIR" == "" ]; then
     echo "ERROR: directory not specified"
     exit 1
 fi
 
-if ! cd $DIR
-then
+if ! cd $DIR; then
     echo "ERROR: unable to chdir to directory '$DIR'"
     exit 2
 fi
 
-if [ "$CAPACITY_LIMIT" == "" ]
-then
-    CAPACITY_LIMIT=95   # default limit
+if [ "$CAPACITY_LIMIT" == "" ]; then
+    CAPACITY_LIMIT=95 # default limit
 fi
 
 CAPACITY=$(df -k . | awk '{gsub("%",""); capacity=$5}; END {print capacity}')
 
-if [ $CAPACITY -gt $CAPACITY_LIMIT ]
-then
-    #
+if [ $CAPACITY -gt $CAPACITY_LIMIT ]; then
     # Get list of files, oldest first.
-    # Delete the oldest files until
-    # we are below the limit. Just
-    # delete regular files, ignore directories.
-    #
-    find $DIR -type f -print0 | xargs -0 ls -tr | grep 'mp4$\|jpg$\|png$\|log$\|json$\|m4a$\|aac$\|flac$\|mp3$\|ogg$\|opus$\|wav$\|mka$\|mkv$\|mpg$\|ogv$\|ts$\|wma$\|wmv$\|webm$\|part$\|ytdl$' | while read FILE
-    do
-        if rm -f "$FILE"
-        then
+    # Delete the oldest files until we are below the limit.
+    # Just delete regular files, ignore directories.
+    find $DIR -type f -iregex ".*.\(mp4\|jpg\|png\|log\|json\|m4a\|aac\|flac\|mp3\|ogg\|opus\|wav\|mka\|mkv\|mpg\|ogv\|ts\|wma\|wmv\|webm\|part\|ytdl\|ffmpeg\.txt\)$" -print0 | xargs -0 ls -tr | while read FILE; do
+        if rm -f "$FILE"; then
             echo "Deleted $FILE"
 
             CAPACITY=$(df -k . | awk '{gsub("%",""); capacity=$5}; END {print capacity}')
 
-            if [ $CAPACITY -le $CAPACITY_LIMIT ]
-            then
+            if [ $CAPACITY -le $CAPACITY_LIMIT ]; then
                 # we're below the limit, so stop deleting
                 exit
             fi
